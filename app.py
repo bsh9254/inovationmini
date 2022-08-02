ea913654e4327c10f25613b93f7be53d66c57dbf
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-08-03 07:20:27 +0900

    Fix bugs

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, render_template, request, url_for, redirect, make_response
 from pymongo import MongoClient
 from flask_jwt_extended import *
+import requests
 
 app = Flask(__name__)
 
@@ -16,14 +17,21 @@ glampediaDB = client["Glampedia"]
 userDB = glampediaDB["User"]
 
 #메인 페이지 라우팅
-@app.route('/')
+@app.route("/", methods = ["GET"])
+@jwt_required(optional = True)
 def home():
-    return render_template('mainpage.html')
+    current_user = get_jwt_identity()
+    user = userDB.find_one({"username": current_user})
+    if user is not None:
+        return render_template("mainpage.html", current_user = user["nickname"])
+    else:
+        return render_template("mainpage.html")
 
 # 메인페이지 GET
 @app.route("/mainpg", methods=["GET"])
+@jwt_required(optional = True)
 def main_get():
-    mainpage=list(db.Glamping_info.find({},{'_id':False}))
+    mainpage=list(glampediaDB.Glamping_info.find({},{'_id':False}))
 
     #tops=list(db.Glamping.find({'star':{"$gte":4.5}},{'_id':False}))
     return jsonify({'mains':mainpage})
@@ -32,8 +40,6 @@ def main_get():
 @app.route("/detailpg")
 def detailinto():
     return render_template("detail.html")
-
-
 
 # 회원가입 페이지 라우팅.
 @app.route("/signup", methods = ["GET"])
@@ -69,7 +75,10 @@ def signup_process():
         "introduction": introduction
     }
     userDB.insert_one(user)
-    return redirect(url_for("login"))
+    access_token = create_access_token(identity = username)
+    response = make_response(redirect("/"))
+    response.set_cookie("access_token_cookie", access_token)
+    return response
 
 # 로그인 처리 라우팅.
 @app.route("/login", methods = ["POST"])
@@ -79,7 +88,7 @@ def login_process():
     user = userDB.find_one({"username": username, "password": password})
     if user is not None:
         access_token = create_access_token(identity = username)
-        response = make_response(render_template("login.html"))
+        response = make_response(redirect("/"))
         response.set_cookie("access_token_cookie", access_token)
         return response
     else:
@@ -98,6 +107,13 @@ def check_redundancy():
         return jsonify({
             "message": "Good to go"
         })
+
+# 로그아웃 처리 라우팅.
+@app.route("/logout", methods = ["GET"])
+def logout():
+    response = make_response(redirect("/"))
+    response.delete_cookie("access_token_cookie")
+    return response
 
 # Authorization 테스트 페이지.
 @app.route("/protected", methods = ["GET"])
