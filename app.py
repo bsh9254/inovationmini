904fe8a2639d2b0e608e0ab65e530168eec59146
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-08-02 09:28:16 +0900

    Add signup and login logics

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 
@@ -16,6 +16,23 @@ def signup():
 @app.route("/login", methods = ["GET"])
 def login():
     return render_template("login.html")
+
+# 회원가입 처리 라우팅.
+@app.route("/signup", methods = ["POST"])
+def signup_process():
+    username = request.form["username"]
+    password = request.form["password"]
+    photo = request.files["photo"]
+    nickname = request.form["nickname"]
+    introduction = request.form["introduction"]
+    return redirect(url_for("main"))
+
+# 로그인 처리 라우팅.
+@app.route("/login", methods = ["POST"])
+def login_process():
+    username = request.form["username"]
+    password = request.form["password"]
+    return redirect(url_for("main"))
 
 if __name__ == "__main__":
     app.run("0.0.0.0", port = 5000, debug = True)
